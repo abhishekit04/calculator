76e08f1c999968473644711492c3ee7ce16cccf6
Abhishek singh <88192065+abhishekit04@users.noreply.github.com>
2021-07-30 18:02:35 +0530

    Add files via upload

---
+++ guess_the_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main () {
+	printf("\t\t::: THE NUMBER GAME :::\t\t \n\n\n\n");
+	int num=50,guess;
+	printf("guess the number\n\n");
+	while(1) {
+		scanf("%d",&guess);
+	
+		if(guess < num) {
+			printf("wrong! guess a larger number\n\n");
+		}
+		
+		else if (guess > num) {
+			printf("wrong! guess a  smaller number\n\n");
+		}
+		
+		else {
+			printf("congratulations! your guess is right\n\n");
+			
+			return ;
+		}	
+	}
+
+	
+	return 0;
+
+}
